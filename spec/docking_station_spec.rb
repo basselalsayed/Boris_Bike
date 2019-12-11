221fde98031d537395e29cd3bdbcb72ac0845ffe
pacifica94 <alsayed.bassel@gmail.com>
2019-12-11 16:17:59 +0000

    Challenge 12

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,4 +1,5 @@
 require "docking_station"
+require 'rspec/expectations'
 
 describe DockingStation do
   let(:station) {DockingStation.new}
@@ -11,11 +12,24 @@ describe DockingStation do
   # end
 
   describe '.working?' do
-    it { expect(station.release_bike.working?).to equal(true) }
+    it { expect(Bike.new.working?).to equal(true) }
   end
 
   describe '.dock' do
     it { expect(station.dock(Bike.new())).to be_a(Bike) }
   end
+
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eq bike
+    end
+  end
+
+  describe 'release bike when bike not docked' do
+    it { expect { station.release_bike }.to raise_error 'No bikes available' }
+  end
+  
 end
 
